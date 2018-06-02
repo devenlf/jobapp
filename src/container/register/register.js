de3f5e88da9fe81f3fa8bea302dc3da99b963ed0
Your Name <1821415702@qq.com>
2018-06-02 23:05:31 +0800

    保存

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,35 +1,62 @@
 import React from 'react';
 import Logo from '../../component/logo/logo.js'
 import {List,InputItem,Radio,WingBlank,WhiteSpace,Button} from 'antd-mobile' 
+import { connect } from 'react-redux'
+import { regisger } from '../../redux/user.redux'
 
-const RadioItem = Radio.RadioItem
+@connect(
+    state=>state.user,
+    {regisger}
+)
+
+
 class Register extends React.Component {
     constructor(props){
         super(props)
         this.state={
+            user:'',
+            pwd:'',
+            repeatpwd:'',
             type:'genius'
         }
     }
 
+    handleChange(key,val){
+        this.setState({
+            [key]:val
+        })
+    }
+    handleRegister(){
+        this.props.regisger(this.state)
+    }
+
     render() {
+        const RadioItem = Radio.RadioItem
         return (
             <div>
                 <Logo></Logo>
                 <List>
-                    <InputItem>用户名</InputItem>
+                    {this.props.msg?<p>我错了大哥</p>:null}
+                    <InputItem onChange={v=>this.handleChange('user',v)}>用户名</InputItem>
                     <WhiteSpace/>
-                    <InputItem>密码</InputItem>
+                    <InputItem onChange={v=>this.handleChange('pwd',v)}>密码</InputItem>
                     <WhiteSpace/>
-                    <InputItem>确认密码</InputItem>
+                    <InputItem onChange={v=>this.handleChange('repeatpwd',v)}>确认密码</InputItem>
                     <WhiteSpace/>
-                    <RadioItem checked={this.state.type==='genius'}>
+                    <RadioItem 
+                    checked={this.state.type==='genius'}
+                    onChange={()=>{this.handleChange('type','genius')}}
+                    >
                         牛人
                     </RadioItem>
-                    <RadioItem checked={this.state.type==='boss'}>
+                    <RadioItem 
+                    checked={this.state.type==='boss'}
+                    onChange={()=>{this.handleChange('type','boss')}}
+                    >
                         BOSS
                     </RadioItem>
                     <WhiteSpace/>
-                    <Button type='primary'>注册</Button>
+                    <Button type='primary' onClick={()=>this.handleRegister()}>注册</Button>
                 </List>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, redirect, Switch } from 'react-router-dom'
-
 import Login from './container/login/login.js';
 import Register from './container/register/register.js';
 import reducers from './reducer.js'
 import AuthRoute from './component/authroute/authroute.js';
 import './config'
 
-const store = createStore(reducers);
+const store = createStore(reducers,compose(
+    applyMiddleware(thunk),
+    // window.devToolsExtension?window.devToolsExtension():f=>f
+));
 
 ReactDom.render((
     <Provider store={store}>
